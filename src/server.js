64786ccbf1a83a37dfa6b310c9465b4aaa55870a
ifarbie <farizrifkyberliano@gmail.com>
2024-07-21 08:39:58 +0700

    feat: implemented postgresql db

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
-const NoteService = require('./services/inMemory/NotesService');
+const NoteService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const noteService = new NoteService();
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
